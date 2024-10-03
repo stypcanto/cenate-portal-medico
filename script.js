a92b20d4b4c32801c2c83da04ed7439f34523adf
Styp Canto <styp611@outlook.com>
2024-10-03 00:34:31 -0500

    Creando la sección Biblioteca

---
+++ script.js
@@ -0,0 +1,67 @@
+// Función que gestiona la visualización del menú
+const Mostrarmenu = (headerToggle, navbarId) => {
+  // Selecciona el botón que alterna el menú usando su ID
+  const toggleBtn = document.getElementById(headerToggle);
+  // Selecciona el elemento del menú usando su ID
+  const nav = document.getElementById(navbarId);
+  
+  // Verifica que ambos parámetros estén definidos
+  if (toggleBtn && nav) { // Cambié headerToggle y navbarId por toggleBtn y nav
+    // Añade un evento de clic al botón de alternancia
+    toggleBtn.addEventListener("click", () => {
+      // Alterna la clase 'show-menu' en el menú para mostrar/ocultar
+      nav.classList.toggle("show-menu");
+      // Alterna la clase 'bx-x' en el botón para cambiar su apariencia
+      toggleBtn.classList.toggle("bx-x");
+    });
+  }
+};
+
+// Llama a la función Mostrarmenu con los IDs específicos
+Mostrarmenu("header-toggle", "navbar");
+
+// Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
+  // Selecciona todos los enlaces de navegación que tienen la clase 'nav__link'
+  const linkcolor = document.querySelectorAll(".nav__link");
+
+  // Función que gestiona el cambio de color del enlace activo
+  function colorLink() {
+    // Elimina la clase 'active' de todos los enlaces
+    linkcolor.forEach((item) => item.classList.remove("active"));
+    // Añade la clase 'active' al enlace que se ha clicado
+    this.classList.add("active");
+  }
+
+  // Asigna un evento de clic a cada enlace de navegación
+  linkcolor.forEach((item) => item.addEventListener("click", colorLink));
+
+  // Selecciona el contenido principal usando su ID
+  const mainContent = document.getElementById("main-content");
+  // Selecciona el contenido de la biblioteca usando su ID
+  const libraryContent = document.getElementById("library-content");
+
+  // Función para mostrar la sección objetivo
+  function showSection(targetSection) {
+    // Ocultar ambas secciones
+    mainContent.style.display = "none";
+    libraryContent.style.display = "none";
+
+    // Mostrar la sección objetivo
+    targetSection.style.display = "block";
+  }
+
+  // Eventos de clic para los enlaces del menú
+  const navLinks = document.querySelectorAll(".nav__dropdown-item, .nav__link");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+      const sectionId = this.getAttribute("href");
+      const targetSection = sectionId === "#section2" ? libraryContent : mainContent;
+      
+      // Mostrar la sección objetivo
+      showSection(targetSection);
+    });
+  });
+});
+
